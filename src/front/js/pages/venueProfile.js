0e8bb57b99b87eaa9a9848fb985a6c1330f36835
WyattChase <esnaruto@yahoo.com>
2023-04-13 00:16:30 +0000

    added messaging modal to profile

--- src/front/js/pages/venueProfile.js
+++ src/front/js/pages/venueProfile.js
@@ -1,11 +1,10 @@
 import React, { useContext, useState } from "react";
-
-
+import { MessagingModal } from "../component/messagingmodal";
 
 import "../../styles/venueProfile.css";
 
-import CalendarPlaceholder from './CalendarPlaceholder.png'
-import MapPlaceholder from './MapPlaceholder.png'
+import CalendarPlaceholder from "./CalendarPlaceholder.png";
+import MapPlaceholder from "./MapPlaceholder.png";
 
 import { Context } from "../store/appContext";
 
@@ -24,7 +23,9 @@ export function VenueProfile() {
               <h2 className="venueName m-0">Venue Name</h2>
             </div>
             <div className="mx-2 pt-1">
-              <button className="btn btn-sm btn-primary">Message</button>
+              <button className="btn btn-sm btn-primary">
+                <MessagingModal />
+              </button>
             </div>
           </div>
           <div className="row mt-0">
@@ -41,63 +42,81 @@ export function VenueProfile() {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, 
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit
+              amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat.
             </p>
-            <p className="my-0"><b>Capacity: </b></p>
-            <p className="my-0"><b>Music Type: </b></p>
-            <p className="my-0"><b>Hiring?: </b></p>
-            <p className="my-0"><b>Pay Rate: </b></p>
-            <p className="my-0"><b>Fee Rate: </b></p>
-            <p className="my-0"><b>Equipment Info: </b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, 
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
-              et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p className="my-0">
+              <b>Capacity: </b>
+            </p>
+            <p className="my-0">
+              <b>Music Type: </b>
+            </p>
+            <p className="my-0">
+              <b>Hiring?: </b>
+            </p>
+            <p className="my-0">
+              <b>Pay Rate: </b>
+            </p>
+            <p className="my-0">
+              <b>Fee Rate: </b>
+            </p>
+            <p className="my-0">
+              <b>Equipment Info: </b>Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+              exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
           </div>
           <div className="row mt-3 px-2">
-            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center"><i className="fa-brands fa-instagram fa-xl"></i></div>
-            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center"><i className="fa-brands fa-tiktok fa-xl"></i></div>
-            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center"><i className="fa-brands fa-facebook fa-xl"></i></div>
-            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center"><i className="fa-brands fa-twitter fa-xl"></i></div>
-            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center"><i className="fa-brands fa-soundcloud fa-xl"></i></div>
-            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center"><i className="fa-brands fa-spotify fa-xl"></i></div>
+            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center">
+              <i className="fa-brands fa-instagram fa-xl"></i>
+            </div>
+            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center">
+              <i className="fa-brands fa-tiktok fa-xl"></i>
+            </div>
+            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center">
+              <i className="fa-brands fa-facebook fa-xl"></i>
+            </div>
+            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center">
+              <i className="fa-brands fa-twitter fa-xl"></i>
+            </div>
+            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center">
+              <i className="fa-brands fa-soundcloud fa-xl"></i>
+            </div>
+            <div className="social-link rounded-circle mx-2 d-flex justify-content-center align-items-center">
+              <i className="fa-brands fa-spotify fa-xl"></i>
+            </div>
           </div>
         </div>
         <div className="row px-2 d-flex justify-content-between align-items-center secondRow">
-            <div className="col-md-5 mx-1">
-              <div className="row d-flex justify-content-between">
-                <div className="col-md m-2 rounded smImage">
-                  Test
-                </div>
-                <div className="col-md m-2 rounded smImage">
-                  Test
-                </div>
-                <div className="col-md m-2 rounded smImage">
-                  Test
-                </div>
-              </div>
-              <div className="row d-flex justify-content-between">
-                <div className="col-md rounded m-2 smImage">
-                  Test
-                </div>
-                <div className="col-md rounded m-2 smImage">
-                  Test
-                </div>
-                <div className="col-md rounded m-2 smImage">
-                  Test
-                </div>
-              </div>
+          <div className="col-md-5 mx-1">
+            <div className="row d-flex justify-content-between">
+              <div className="col-md m-2 rounded smImage">Test</div>
+              <div className="col-md m-2 rounded smImage">Test</div>
+              <div className="col-md m-2 rounded smImage">Test</div>
             </div>
-            <div className="col-md-6"><img className="calendar" src={CalendarPlaceholder} /><img className="calendar mx-5" src={CalendarPlaceholder} /></div>
+            <div className="row d-flex justify-content-between">
+              <div className="col-md rounded m-2 smImage">Test</div>
+              <div className="col-md rounded m-2 smImage">Test</div>
+              <div className="col-md rounded m-2 smImage">Test</div>
+            </div>
           </div>
-          <div className="row rounded"><img src={MapPlaceholder} /></div>
+          <div className="col-md-6">
+            <img className="calendar" src={CalendarPlaceholder} />
+            <img className="calendar mx-5" src={CalendarPlaceholder} />
+          </div>
+        </div>
+        <div className="row rounded">
+          <img src={MapPlaceholder} />
+        </div>
       </div>
-           
-      </div>
+    </div>
   );
 }
