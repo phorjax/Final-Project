16ea7658bcb5678a1cf3d23e3c27e702ea7cd31c
WyattChase <esnaruto@yahoo.com>
2023-04-20 11:42:44 +0000

    added direct messaging application test

---
+++ chat-application-example/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DirectMessaging from "./DirectMessaging";
+
+//Using arrow function component
+const App = () => {
+  return (
+    
+    
+    <DirectMessaging/>
+
+  );
+};
+export default App;
